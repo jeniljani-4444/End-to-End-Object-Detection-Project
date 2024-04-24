a22fb79e51e7b6c2a8b111c4e3e10806f236972b
Jenil Jani <jeniljani14@gmail.com>
2024-04-25 00:26:30 +0530

    requirements.txt added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Jenil Jani',
+    author_email= 'jeniljani14@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
